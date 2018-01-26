c187bb0ac764efa8ace430b97f60fa6f806c7503
gaofan <1125356092@qq.com>
2018-01-26 20:15:56 +0800

    ads

--- js/register.js
+++ js/register.js
@@ -6,109 +6,53 @@ window.onload= function(){
 	new GetName().init();	    //左下角时间
 	new CarBoard().init();      //head购物篮广告
 	new ShowUl();               //main点击左侧按钮出现子元素ul
-	new Verify().init()         //表单验证
+	new register().init();      
 }
-//表单验证
-	function Verify(){
-//		* 请在标示的项目输入正确资料：
-//		称谓及姓名 - 请输入此项资料
-//		所在地 - 请选择你的所在地
-//		电邮地址 - 请输入此项资料
-//		建立密码 - 密码须由 6-12 个英文及数字组成，大小写有别。
-//		使用条款 - 请在方格内打勾表示接受有关的条款。
-//		图像核证 - 请按图像显示输入字符
+//登录验证
+	function register(){
 		this.init = function(){
-			this.flagName = true;
-			this.flagAddress = true;
-			this.flagEmile = true;
-			this.flagPwd = true;
-			this.flagQpwd = true;
-			this.flagProvision = true;
-			$("#formBox").submit(function(){
-				if( this.flagName&&this.flagAddress&&this.flagEmile&&this.flagPwd&&
-					this.flagQpwd&&this.flagProvision){
-						alert("注册成功")
-						var obj = {
-							"name": $("#userName").val(),
-							"pwd": $("#userPwd").val(),
-							"email": $("#userEmile").val()
-						}
-//						$.ajax({
-//							type : "get",
-//							url : "../json/userinfo.json",
-//							dataType : "json",
-//							data : obj,
-//							success : function(json){
-//								console.log( json )
-//								json.list.push( obj )
-////								$(json).list.html( json.list.push( obj ) )
-//								console.log( json )
-//							}
-//						})
-						document.cookie="user="+JSON.stringify( obj )
-						console.log( document.cookie );
-						return true;
+			$("#enBtn").click(function(){
+				var obj = this.getCookie("user");
+				if( $("#useEmile").val() != "" && $("#useEmile").val() == obj.email 
+					&& $("#usePwd").val() != "" && $("#usePwd").val() == obj.pwd
+				){
+					$("#enterEM1,#enterEM2").css("display","none")
+					alert("登录成功")
+					location.href = "../index.html";
+					return;
+				}
+					if( $("#useEmile").val() != obj.email ){
+						$("#enterEM1").css("display","block")
+						$("#enterEM1 em").html("请输入正确的用户名")
 					}else{
-						$("<dt>").html("请在标示的项目内输入正确信息 : ").prependTo("#formEnter");
-						$("html,body").animate({"scrollTop":200},1000)
-						return false;
+						$("#enterEM1").css("display","none")
+						$("#enterEM1 em").html("")
 					}
-			}.bind(this))
-			
-			$("#subMit").click(function(){
-				$("#formEnter").find("dd").remove();
-				$("#formEnter").find("dt").remove();
-				//称位
-				if( $("#userNamed").val()=="--" || $("#userName").val()=="" ){
-					$("<dd>").html("称谓及姓名 - 请输入此项资料").appendTo( "#formEnter" )
-					this.flagName = false;
-				}else{
-					this.flagName = true;
+					if( $("#usePwd").val() != obj.pwd ){
+						$("#enterEM2").css("display","block")
+						$("#enterEM2em").html("请输入正确的密码")
+					}else{
+						$("#enterEM2").css("display","none")
+						$("#enterEM2 em").html("")
+					}
+			}.bind(this) )
+		};
+		this.getCookie = function(key){
+			cookie_info = document.cookie;
+			if (cookie_info) {
+				list = cookie_info.replace(/;\s/g,";").split(';');
+				for (var i=0;i<list.length;i++) {
+					item = list[i].split('=');
+					if (item[0] == key) {
+						oldCookie = item[1];
+						return JSON.parse(oldCookie);
+						}
+					}
+				return [];	
 				}
-				//所在地 - 请选择你的所在地
-				if( $("#userAddress").val()=="--请选择--" ){
-					$("<dd>").html("所在地 - 请选择你的所在地").appendTo( "#formEnter" )
-					this.flagAddress = false;
-				}else{
-					this.flagAddress = true;
-				}
-				//邮箱
-				var regEmlie = /^\w{8,}@\w{2,}\.\w{2,}$/
-				if( regEmlie.test( $("#userEmile").val() ) ){
-					this.flagEmile = true;
-				}else if( $("#userEmile").val() != $("#QuserEmile").val()){
-					alert(" ")
-					$("<dd>").html("电邮地址 - 请输入此项资料").appendTo( "#formEnter" )
-					this.flagEmile = false;
-				}
-				else{
-					$("<dd>").html("电邮地址 - 请输入此项资料").appendTo( "#formEnter" )
-					this.flagEmile = false;
-				}
-				var redPwd = /^.{6,}$/
-				if( redPwd.test( $("#userPwd").val() ) ){
-					this.flagPwd = true;
-				}else{
-					$("<dd>").html("密码须由 6-12 个英文及数字组成，大小写有别").appendTo( "#formEnter" )
-					this.flagPwd = false;
-				}
-				//确认密码
-				if( $("#userPwd").val() == $("#QuserPwd").val() ){
-					this.flagQpwd = true;
-				}else{
-					$("<dd>").html("两次密码输入不一致").appendTo( "#formEnter" )
-					this.flagQpwd = false;
-				}
-				//使用条款 - 请在方格内打勾表示接受有关的条款。
-				if( $("#provision").is(":checked")){
-					this.flagProvision = true;
-				}else{
-					this.flagProvision = false;
-					$("<dd>").html("使用条款 - 请在方格内打勾表示接受有关的条款。").appendTo( "#formEnter" )
-				}
-			}.bind(this))
+			return [];
+			}
 		}
-	}
 //点击左侧按钮出现子元素ul
 	function ShowUl(){
 		$("li").click(function(){
